2d0088504b1160f0cb7bee25178e6f986a2980b6
Mikko Kokkonen <mikko.kokkonen@outlook.com>
2018-09-05 19:31:58 +0300

    Test script for ARM template testing

---
+++ writedatetime.ps1
@@ -0,0 +1,6 @@
+﻿$targetDir = "C:\Temp"
+$targetFile = "datetime.txt"
+if (-Not (Test-Path -Path $targetDir)) {
+    New-Item -Name $targetDir -ItemType Directory
+}
+(Get-Date).DateTime | Out-File "$targetDir\$targetFile" -Append
