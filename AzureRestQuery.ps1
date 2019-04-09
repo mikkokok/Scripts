dfac9f1dc21e29b2a04b7ed6372a2812da32bb8b
Mikko Kokkonen <mikko.kokkonen@olen.to>
2019-04-09 13:14:53 +0300

    Create AzureRestQuery.ps1

---
+++ AzureRestQuery.ps1
@@ -0,0 +1,18 @@
+$azureRMUri = "https://management.azure.com/subscriptions/"
+$providerUri = "/providers/microsoft.insights/logprofiles/default?api-version=2016-03-01"
+
+$subscriptionId = (Get-AzContext).Subscription.Id
+$tenantId = (Get-AzSubscription -SubscriptionId $subscriptionId).TenantId
+$currentContext = Get-AzContext
+$accessToken = $currentContext.TokenCache.ReadItems() | Where-Object { $_.TenantId -eq $tenantId } | Sort-Object -Property ExpiresOn -Descending | Select-Object -First 1
+
+$uri = $azureRMUri + $subscriptionId + $providerUri
+$params = @{
+    ContentType = 'application/json'
+    Headers = @{
+        "Authorization" = "Bearer " + $accessToken.AccessToken
+        }
+    Method = 'GET'
+    URI = $uri
+}
+Invoke-RestMethod @params | ConvertTo-Json
